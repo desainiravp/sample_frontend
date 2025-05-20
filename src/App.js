cbfc1954ac8b41997d2f596501029cf67fcb8384
desainiravp <125334778+desainiravp@users.noreply.github.com>
2025-05-20 15:34:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('Loading from Lambda...');
+
+  useEffect(() => {
+    fetch('https://p9gv8wxkve.execute-api.us-east-1.amazonaws.com/prod/api')
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(err => setMessage('Error calling Lambda: ' + err));
+  }, []);
+
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+    <div className="App">
       <h1>Hello from AWS Amplify!</h1>
       <p>This is a sample React frontend.</p>
+      <h3>Lambda Response:</h3>
+      <p>{message}</p>
     </div>
   );
 }
