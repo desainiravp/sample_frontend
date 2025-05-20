729c6e4b0c5960d798c209acef27fbd4e320bcd8
desainiravp <125334778+desainiravp@users.noreply.github.com>
2025-05-20 17:05:22 +0530

    Latest App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Hello from AWS Amplify!</h1>
+      <h1>Hello from AWS Amplify!!!</h1>
       <p>This is a sample React frontend.</p>
     </div>
   );
